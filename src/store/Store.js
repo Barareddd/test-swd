7aaece2b8f4a2a378a7b572067df42d6b2a7d861
Barared <best.siraboonchai@gmail.com>
2024-04-19 18:38:30 +0700

    add: loadStateFromLocalStorage

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,12 +2,19 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import personsReducer from "./personsSlice";
-import { saveStateToLocalStorage } from "../localStorage";
+import {
+  saveStateToLocalStorage,
+  loadStateFromLocalStorage,
+} from "../localStorage";
+
+const persistedState = loadStateFromLocalStorage();
 
 const store = configureStore({
   reducer: {
     persons: personsReducer,
   },
+
+  preloadedState: persistedState,
 });
 
 store.subscribe(() => {
